95b3b9f58b4e6abed68a727574ba25a76c29e65f
bayoumi <abdelrahmanbayoumi1@gmail.com>
2020-07-25 18:50:56 +0200

    first commit

---
+++ src/main/java/com/bayoumi/main/Launcher.java
@@ -0,0 +1,11 @@
+package com.bayoumi.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
